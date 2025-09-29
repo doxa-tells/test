88fcea7789778da3ebe88add02dd700b648f97e2
dtelmanov <telmanov.darkhan@gmail.com>
2025-09-29 11:59:03 +0500

    fav

---
+++ web/catalog/app/favorites/page.tsx
@@ -0,0 +1,8 @@
+export default function FavoritesPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Моя подборка</h1>
+      <p>Раздел находится в разработке.</p>
+    </div>
+  );
+}
